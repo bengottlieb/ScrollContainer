953ab61c32e45bd21066638116ec5238e817a0bc
Ben Gottlieb <ben@standalone.com>
2023-10-29 06:52:02 -0500

    Initial Commit

---
+++ ScrollContainer/ScrollContainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollContainerApp.swift
+//  ScrollContainer
+//
+//  Created by Ben Gottlieb on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
